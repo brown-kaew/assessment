c32e0d6cec0bf65b80fc2fe7a39e1d9c831334e7
Kaew Tibkham <kaewtibkham@gmail.com>
2023-01-05 01:12:26 +0700

    Add UpdateExpenseById

--- expense/handler.go
+++ expense/handler.go
@@ -12,6 +12,7 @@ type Handler interface {
 	InitRoutes(e *echo.Echo)
 	CreateNewExpense(expense *Expense) error
 	GetExpenseById(id string) (*Expense, error)
+	UpdateExpenseById(expense *Expense) error
 }
 
 type handler struct {
@@ -27,6 +28,7 @@ func NewHandler(db *sql.DB) Handler {
 func (h *handler) InitRoutes(e *echo.Echo) {
 	e.POST("/expenses", h.createNewExpenseHandler())
 	e.GET("/expenses/:id", h.getExpenseHandler())
+	e.PUT("/expenses/:id", h.updateExpenseHandler())
 }
 
 func (h *handler) createNewExpenseHandler() echo.HandlerFunc {
@@ -48,6 +50,23 @@ func (h *handler) getExpenseHandler() echo.HandlerFunc {
 	return func(c echo.Context) error {
 		id := c.Param("id")
 		expense, err := h.GetExpenseById(id)
+		if err != nil {
+			return err
+		}
+		return c.JSON(http.StatusOK, expense)
+	}
+}
+
+func (h *handler) updateExpenseHandler() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		var expense Expense
+		err := c.Bind(&expense)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+		expense.Id = c.Param("id")
+
+		err = h.UpdateExpenseById(&expense)
 		if err != nil {
 			return err
 		}
@@ -82,10 +101,40 @@ func (h *handler) GetExpenseById(id string) (*Expense, error) {
 		return nil, echo.NewHTTPError(http.StatusNotFound, "Expense not found")
 	}
 
-	var e Expense
-	err = row.Scan(&e.Id, &e.Title, &e.Amount, &e.Note, pq.Array(&e.Tags))
+	var expense Expense
+	err = row.Scan(&expense.Id, &expense.Title, &expense.Amount, &expense.Note, pq.Array(&expense.Tags))
 	if err != nil {
 		return nil, err
 	}
-	return &e, nil
+	return &expense, nil
+}
+
+func (h *handler) UpdateExpenseById(expense *Expense) error {
+	stmt, err := h.db.Prepare(`
+	UPDATE expenses
+	SET
+		title=$2,
+		amount=$3,
+		note=$4,
+		tags=$5
+	WHERE id=$1
+	`)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Cannot prepare statment")
+	}
+
+	var res sql.Result
+	res, err = stmt.Exec(expense.Id, expense.Title, expense.Amount, expense.Note, pq.Array(&expense.Tags))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Cannot update expense: "+err.Error())
+	}
+
+	row, err := res.RowsAffected()
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "Cannot update expense: "+err.Error())
+	}
+	if row == 0 {
+		return echo.NewHTTPError(http.StatusNotFound, "Expense not found")
+	}
+	return nil
 }
