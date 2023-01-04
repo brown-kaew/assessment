f39e654eaecf30dd6014c23c1463a7071d616764
Kaew Tibkham <kaewtibkham@gmail.com>
2023-01-05 01:33:03 +0700

    Add TestUpdateExpenseById with seedExpenses

--- expense/handler_it_test.go
+++ expense/handler_it_test.go
@@ -91,6 +91,34 @@ func setUpNoDB() (config.Config, func()) {
 	}
 }
 
+func seedExpenses(t *testing.T, config config.Config) string {
+	reqBody := `{
+		"title": "strawberry smoothie",
+		"amount": 79,
+		"note": "night market promotion discount 10 bath",
+		"tags": [
+		  "food",
+		  "beverage"
+		]
+	  }`
+	url := fmt.Sprintf("http://localhost%s/expenses", config.Port)
+	req, err := http.NewRequest(http.MethodPost, url, strings.NewReader(reqBody))
+	assert.NoError(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	client := http.Client{}
+
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
+	byteBody, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	resp.Body.Close()
+
+	var expense expense.Expense
+	err = json.Unmarshal(byteBody, &expense)
+
+	return expense.Id
+}
+
 func TestHealth(t *testing.T) {
 	config, teardown := setUp()
 	defer teardown()
@@ -285,5 +313,71 @@ func TestGetExpenseById_NoIdIsFound_ShouldGetNotFound(t *testing.T) {
 		assert.Empty(t, expense.Amount)
 		assert.Empty(t, expense.Note)
 		assert.Empty(t, expense.Tags)
+	}
+}
+
+func TestGetExpenseById_NoDbConn_ShouldGetInternalServerError(t *testing.T) {
+	config, teardown := setUpNoDB()
+	defer teardown()
+
+	// Arrange
+	reqBody := ``
+	url := fmt.Sprintf("http://localhost%s/expenses/1", config.Port)
+	req, err := http.NewRequest(http.MethodGet, url, strings.NewReader(reqBody))
+	assert.NoError(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	client := http.Client{}
+
+	//Act
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
+	byteBody, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	resp.Body.Close()
+
+	//Assert
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusInternalServerError, resp.StatusCode)
+		assert.Contains(t, strings.TrimSpace(string(byteBody)), `"message":`)
+	}
+}
+
+func TestUpdateExpenseById_Success(t *testing.T) {
+	config, teardown := setUp()
+	defer teardown()
+
+	// Arrange
+	id := seedExpenses(t, config)
+	reqBody := `{
+		"title": "MaMa",
+		"amount": 5,
+		"note": "Yummy",
+		"tags": [
+		  "food"
+		]
+	  }`
+	url := fmt.Sprintf("http://localhost%s/expenses/%s", config.Port, id)
+	req, err := http.NewRequest(http.MethodPut, url, strings.NewReader(reqBody))
+	assert.NoError(t, err)
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	client := http.Client{}
+
+	//Act
+	resp, err := client.Do(req)
+	assert.NoError(t, err)
+	byteBody, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	resp.Body.Close()
+
+	//Assert
+	var expense expense.Expense
+	err = json.Unmarshal(byteBody, &expense)
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, resp.StatusCode)
+		assert.Equal(t, id, expense.Id)
+		assert.Equal(t, "MaMa", expense.Title)
+		assert.Equal(t, float64(5), expense.Amount)
+		assert.Equal(t, "Yummy", expense.Note)
+		assert.Equal(t, []string{"food"}, expense.Tags)
 	}
 }
